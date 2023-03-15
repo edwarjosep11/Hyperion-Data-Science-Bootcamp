96832bffc8e314e60a0e6fe4b557f5813d1cdab1
Edwar Josep Benavente Ingiltupa <ccus.edwar.benavente@gmail.com>
2023-03-15 15:22:18 +0000

    Use of variables
    First program and simple use of variables

---
+++ T1_Use_of_variables/hello_world.py
@@ -0,0 +1,7 @@
+username = input('Please introduce your name: ')
+print('Hello! ', username)
+age = input('Introduce your age: ')
+print('Hello ', username, 'your age is: ', age)
+
+
+print('Hello World!')
---
+++ T1_Use_of_variables/menu.py
@@ -0,0 +1,8 @@
+item1 = input('Select your first order: ')
+item2 = input('Select your second order: ')
+item3 = input('Select your third order: ')
+
+print('This is your order confirmation! You have ordered:')
+print('-', item1)
+print('-', item2)
+print('-', item3)
